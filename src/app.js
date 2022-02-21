0f5b6026c62ffac37db55194fdd1f765e026f217
Aphro <ilboudosouleymane4@gmail.com>
2022-02-21 21:23:33 +0000

    premier modif

---
+++ src/app.js
@@ -0,0 +1,14 @@
+
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="container">
+          <img src="images/WhatsApp Image 2022-02-21 at 17.06.37.jpeg"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
